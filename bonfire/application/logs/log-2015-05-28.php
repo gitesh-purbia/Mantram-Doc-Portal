ef32ccb56a37275affd5abe17251d9768e9653d6
pankaj <pankaj.choudhary1020@gmail.com>
2015-06-20 17:17:45 +0530

    delete log files

--- bonfire/application/logs/log-2015-05-28.php
+++
@@ -1,6 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2015-05-28 08:47:35 --> 404 Page Not Found --> drug_stores/images
-ERROR - 2015-05-28 08:47:45 --> 404 Page Not Found --> 
-ERROR - 2015-05-28 08:48:28 --> 404 Page Not Found --> 
-ERROR - 2015-05-28 08:48:35 --> 404 Page Not Found --> 
